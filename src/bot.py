1ac505b916f5bbc8c096085c2a266712dd5a47e4
Accendino <other2180@gmail.com>
2021-01-09 23:41:59 +0100

    Fix

--- src/bot.py
+++ src/bot.py
@@ -4,11 +4,11 @@ from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv()
 
-bot = commands.Bot(command_prefix=process.env.PREFIX)
+bot = commands.Bot(command_prefix=os.getenv('PREFIX'))
 
 @bot.event
 async def on_ready():
   print(f'{bot.user} has logged in.')
   bot.load_extension('cogs.music')
 
-bot.run(process.env.BOT_TOKEN)
+bot.run(os.getenv('BOT_TOKEN'))
